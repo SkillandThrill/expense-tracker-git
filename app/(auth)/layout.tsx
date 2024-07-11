bdd5218b37ffede75010ab2daa6937e43a01db8f
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-11 14:02:43 +0530

    adding the logo for the application and gave the title of the app

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,10 @@
 import React, { ReactNode } from 'react'
+import Logo from '@/components/Logo'
 
 const layout = ({children} :{children:ReactNode}) => {
   return (
     <div className='relative flex h-screen flex-col items-center justify-center'>
+      <Logo/>
         <div className='mt-12'>
             {children}
         </div>

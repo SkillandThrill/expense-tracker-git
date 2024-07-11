39b4d54e48d662e35bd5494df18611e8602650f1
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-11 13:51:38 +0530

    implemented authentication with clerk added the route groupsfor sign in and sign up

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react'
+
+const layout = ({children} :{children:ReactNode}) => {
+  return (
+    <div className='relative flex h-screen flex-col items-center justify-center'>
+        <div className='mt-12'>
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export default layout

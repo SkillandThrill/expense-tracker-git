792f3b4c1fc13e91efc717cbea0de1ef68ae32d7
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-12 15:16:16 +0530

    creating a http api that returns users settings for crud operations using tanstackQuery and for validation Zod schema.. to implement devtools into app wrapping the app with React-query Provider.. also added the react query devtools on the app

--- components/providers/RootProviders.tsx
+++ components/providers/RootProviders.tsx
@@ -2,9 +2,13 @@
 
 import { ThemeProvider } from 'next-themes'
 import React, { ReactNode } from 'react'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
 
 function RootProviders({children}:{children:ReactNode}) {
+  const [queryClient] = React.useState(() => new QueryClient({}));
   return (
+    <QueryClientProvider client={queryClient}>
     <ThemeProvider 
         attribute='class'
         defaultTheme='dark'
@@ -13,6 +17,8 @@ function RootProviders({children}:{children:ReactNode}) {
     >
       {children}
     </ThemeProvider>
+    <ReactQueryDevtools initialIsOpen={false}/>
+    </QueryClientProvider>
   )
 }
 

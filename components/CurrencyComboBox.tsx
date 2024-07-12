b0981ea313d48330809aa05ff0e7de79d37ba6ca
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-12 14:26:21 +0530

    added the currencies to the page through currencies.ts with their locale values and updated the combobox component to accept the currencies as in values.. updated the command ui component to select the currencies on wizard page

--- components/CurrencyComboBox.tsx
+++ components/CurrencyComboBox.tsx
@@ -22,39 +22,14 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import { Currencies, Currency } from "@/lib/currencies"
 
-type Status = {
-  value: string
-  label: string
-}
 
-const statuses: Status[] = [
-  {
-    value: "backlog",
-    label: "Backlog",
-  },
-  {
-    value: "todo",
-    label: "Todo",
-  },
-  {
-    value: "in progress",
-    label: "In Progress",
-  },
-  {
-    value: "done",
-    label: "Done",
-  },
-  {
-    value: "canceled",
-    label: "Canceled",
-  },
-]
 
 export function CurrencyComboBox() {
   const [open, setOpen] = React.useState(false)
   const isDesktop = useMediaQuery("(min-width: 768px)")
-  const [selectedStatus, setSelectedStatus] = React.useState<Status | null>(
+  const [selectedOption, setSelectedOption] = React.useState<Currency | null>(
     null
   )
 
@@ -62,12 +37,12 @@ export function CurrencyComboBox() {
     return (
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
-          <Button variant="outline" className="w-[150px] justify-start">
-            {selectedStatus ? <>{selectedStatus.label}</> : <>+ Set status</>}
+          <Button variant="outline" className="w-full justify-start">
+            {selectedOption ? <>{selectedOption.label}</> : <>Set currency</>}
           </Button>
         </PopoverTrigger>
         <PopoverContent className="w-[200px] p-0" align="start">
-          <StatusList setOpen={setOpen} setSelectedStatus={setSelectedStatus} />
+          <OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
         </PopoverContent>
       </Popover>
     )
@@ -76,44 +51,44 @@ export function CurrencyComboBox() {
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>
-        <Button variant="outline" className="w-[150px] justify-start">
-          {selectedStatus ? <>{selectedStatus.label}</> : <>+ Set status</>}
+        <Button variant="outline" className="w-full justify-start">
+          {selectedOption ? <>{selectedOption.label}</> : <>Set currency</>}
         </Button>
       </DrawerTrigger>
       <DrawerContent>
         <div className="mt-4 border-t">
-          <StatusList setOpen={setOpen} setSelectedStatus={setSelectedStatus} />
+          <OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
         </div>
       </DrawerContent>
     </Drawer>
   )
 }
 
-function StatusList({
+function OptionList({
   setOpen,
-  setSelectedStatus,
+  setSelectedOption,
 }: {
   setOpen: (open: boolean) => void
-  setSelectedStatus: (status: Status | null) => void
+  setSelectedOption: (status: Currency | null) => void
 }) {
   return (
     <Command>
-      <CommandInput placeholder="Filter status..." />
+      <CommandInput placeholder="Filter currency..." />
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
         <CommandGroup>
-          {statuses.map((status) => (
+          {Currencies.map((currency:Currency) => (
             <CommandItem
-              key={status.value}
-              value={status.value}
+              key={currency.value}
+              value={currency.value}
               onSelect={(value) => {
-                setSelectedStatus(
-                  statuses.find((priority) => priority.value === value) || null
+                setSelectedOption(
+                  Currencies.find((priority) => priority.value === value) || null
                 )
                 setOpen(false)
               }}
             >
-              {status.label}
+              {currency.label}
             </CommandItem>
           ))}
         </CommandGroup>

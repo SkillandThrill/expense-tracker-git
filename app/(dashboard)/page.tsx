504a0193df76faef1825de9c655917d8c09575b9
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-13 12:53:33 +0530

    creating dashboard page intro for that created the Loading component in dashboard route group and returned the null value which will keep the navbar intact while reloading the page..

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,32 @@
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation';
 import React from 'react'
+import prisma from '@/lib/prisma';
 
-function page() {
+async function page() {
+  const user = await currentUser();;
+  if(!user){
+      redirect("/sign-in");
+  }
+
+  const userSettings = await prisma.userSettings.findUnique({
+    where:{
+      userId: user.id,
+    },
+  });
+
+  if(!userSettings){
+    redirect("/wizard");
+  }
   return (
-    <div>
-      Hello
+    <div className="h-full bg-background ">
+      <div className="border-b bg-card">
+        <div className='container flex items-center flex-wrap justify-between gap-6 py-8'>
+          <p className='text-3xl font-bold'>
+            Hello, {user.firstName}! 👋
+          </p>
+        </div>
+      </div>
     </div>
   )
 }

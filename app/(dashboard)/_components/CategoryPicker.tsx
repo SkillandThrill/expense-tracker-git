c3d87ae4385526cfff54c3103bc532d4bc3e3058
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-13 15:42:14 +0530

    created a popover to show the categories and added the button-ui to select the categories to add the categories created a categoryRow component within the component.. passed the value in like icon and img for the categories.. customizing the command-ui component for the selecting option of the categories..

--- app/(dashboard)/_components/CategoryPicker.tsx
+++ app/(dashboard)/_components/CategoryPicker.tsx
@@ -1,18 +1,73 @@
 "use client"
 
 import { TransactionType } from '@/lib/types'
+import { useQuery } from '@tanstack/react-query'
+import { Category } from '@prisma/client'
 import React from 'react'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { Button } from '@/components/ui/button'
+import { Command, CommandInput } from '@/components/ui/command'
 
 interface Props{
     type: TransactionType
 }
 
 function CategoryPicker({type}:Props) {
+
+  const [open,SetOpen] = React.useState(false);
+  const [value,SetValue] = React.useState("");
+
+
+  const categoriesQuery = useQuery({
+    queryKey:["categories",type],
+    queryFn:() => fetch(`/api/categories?type=${type}`).then(res => res.json()),
+  });
+
+  const selectCategory = categoriesQuery.data?.find(
+    (category: Category)=> category.name === value
+  );
+
   return (
-    <div>
-      
-    </div>
+    <Popover 
+      open={open}
+      onOpenChange={SetOpen}
+    >
+      <PopoverTrigger
+        asChild
+      >
+        <Button 
+          variant={"outline"}
+          role='combobox'
+          aria-expanded={open}
+          className='w-[200px] justify-between'
+        >
+          {selectCategory ? <CategoryRow category={selectCategory}/> : "Select category"}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        className='w-[200px] p-0'
+      >
+        <Command onSubmit={e =>{
+          e.preventDefault()
+        }}>
+          <CommandInput placeholder='Search category...'/>
+        </Command>
+      </PopoverContent>
+    </Popover>
   )
 }
 
 export default CategoryPicker
+
+function CategoryRow({category}:{category:Category}) {
+  return (
+    <div className="flex items-center gap-2">
+      <span role='img'>
+        {category.icon}
+      </span>
+      <span>
+        {category.name}
+      </span>
+    </div>
+  );
+}

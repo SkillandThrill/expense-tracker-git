fd623ce72c28e84f20fbb8eb54e9bc2053e28457
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-13 15:11:11 +0530

    created a schema for the form items to collect information and imported the ui-form-component..created multiple form fields for the description and the amount..for category creating a new component as category-picker witadding the props as type.. we are going to list user categories for that we are defined api for it. inthat api we defined searchParam

---
+++ app/(dashboard)/_components/CategoryPicker.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { TransactionType } from '@/lib/types'
+import React from 'react'
+
+interface Props{
+    type: TransactionType
+}
+
+function CategoryPicker({type}:Props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CategoryPicker

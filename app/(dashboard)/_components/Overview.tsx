844625ac7c26d6b5826b51d970e47e346e7aa237
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-16 12:45:14 +0530

    styling the overview page and added daterangepicler component with additional properties. . not allowing query from one year or two for that creted constants.ts in lib which will limit the days to retrieve the data.

--- app/(dashboard)/_components/Overview.tsx
+++ app/(dashboard)/_components/Overview.tsx
@@ -1,9 +1,12 @@
 "use client"
 
 
+import { DateRangePicker } from '@/components/ui/date-range-picker';
+import { MAX_DATE_RANGE_DAYS } from '@/lib/constants';
 import { UserSettings } from '@prisma/client'
-import { startOfMonth } from 'date-fns';
+import { differenceInDays, startOfMonth } from 'date-fns';
 import React, { useState } from 'react'
+import { toast } from 'sonner';
 
 function Overview({userSettings}:{userSettings: UserSettings}) {
 const [dateRange, setDateRange] = useState<{from:Date; to:Date}>({
@@ -11,9 +14,32 @@ const [dateRange, setDateRange] = useState<{from:Date; to:Date}>({
     to : new Date(),
 });
   return (
-    <div>
-      
-    </div>
+    <>
+        <div className="container flex flex-wrap items-start justify-between gap-2 py-6">
+            <h2 className='text-3xl font-bold'>
+                Overview
+            </h2>
+            <div className="flex items-center gap-3">
+                <DateRangePicker
+                    initialDateFrom={dateRange.from}
+                    initialDateTo={dateRange.to}
+                    showCompare ={false}
+                    onUpdate={values =>{
+                        const {from,to}= values.range;
+                        // we update date range only if oth dates are set
+                        if(!from || !to ) return;
+
+                        if(differenceInDays(to,from) > MAX_DATE_RANGE_DAYS){
+                            toast.error(`The selected date range is too big. Max allowed range is ${MAX_DATE_RANGE_DAYS} days!`);
+                            return;
+                        }
+
+                        setDateRange({from,to});
+                    }}
+                />
+            </div>
+        </div>
+    </>
   )
 
   

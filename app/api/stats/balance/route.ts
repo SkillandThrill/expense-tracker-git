550fdb006a7479cba4cf81c9e2922110b1009edd
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-16 13:19:42 +0530

    now heading towards to create the income expense and balance cards to show the stats for which created a api route of balance which will have the total amount and total income for the selected period and so the search params wil be from and to so extracted them and validated

---
+++ app/api/stats/balance/route.ts
@@ -0,0 +1,13 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export async function GET(request :Request){
+    const user = await currentUser();
+    if(!user){
+        redirect ("/sign-in")
+    }
+    
+    const {searchParams} = new URL(request.url);
+    const from  = searchParams.get("from");
+    const to = searchParams.get("to");
+}

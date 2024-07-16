35c37b292389015565f6c5ff816cd82f9b162176
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-16 13:42:27 +0530

    to validate in api route used queryParams as OverviewQuerySchema , added condition if there is no queryparams then return error with json response.. to get the stats created helper function which exports the type of balance stats. due to the type is ReturnType we will have type inferrence for the api calls

--- app/api/stats/balance/route.ts
+++ app/api/stats/balance/route.ts
@@ -1,5 +1,7 @@
+import { OverviewQuerySchema } from "@/schema/overview";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import data from '@emoji-mart/data';
 
 export async function GET(request :Request){
     const user = await currentUser();
@@ -11,5 +13,27 @@ export async function GET(request :Request){
     const from  = searchParams.get("from");
     const to = searchParams.get("to");
 
-    
+    const queryParams = OverviewQuerySchema.safeParse({
+        from,to
+    });
+
+    if(!queryParams.success){
+        return Response.json(queryParams.error.message,{
+            status:400,
+        })
+    }
+
+    const stats = await getBalanceStats(
+        user.id,
+        queryParams.data.from,
+        queryParams.data.to,
+    );
+
+    return Response.json(stats)
+}
+
+export type GetBalanceStatsResponseType = Awaited<ReturnType<typeof getBalanceStats> >;
+
+async function getBalanceStats(userId:string,from:Date, to:Date ){
+
 }

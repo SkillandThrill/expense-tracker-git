0e17a9a95662e7407d7b2536221e70b7a22445dd
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-16 13:30:20 +0530

    to check the date range is selected than the max allowed date range for that within the schema created overview file which validates and exports from and to add a custom check

--- app/api/stats/balance/route.ts
+++ app/api/stats/balance/route.ts
@@ -10,4 +10,6 @@ export async function GET(request :Request){
     const {searchParams} = new URL(request.url);
     const from  = searchParams.get("from");
     const to = searchParams.get("to");
+
+    
 }

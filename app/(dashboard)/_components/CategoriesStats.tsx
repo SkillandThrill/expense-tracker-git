63cf0bc2e427538fb3da0d61ea4c4fccde7ca76d
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-17 23:59:16 +0530

    to call the api with the react query with the fetch library so created variables like statsQuery and useMemo to get the stats and started stylling the page within added skeletonWrapper component and created new component to actucally show the data on, added card component from the components-ui added detais if there is no record in the selected period.

--- app/(dashboard)/_components/CategoriesStats.tsx
+++ app/(dashboard)/_components/CategoriesStats.tsx
@@ -1,7 +1,13 @@
 "use client"
 
-import React from 'react'
+import React, { useMemo } from 'react'
 import { UserSettings } from '@prisma/client';
+import { useQuery } from '@tanstack/react-query';
+import { DateToUTCDate, GetFormatterForCurrency } from '@/lib/helpers';
+import SkeletonWrapper from '@/components/SkeletonWrapper';
+import { TransactionType } from '@/lib/types';
+import { GetCategoriesStatsResponseType } from '@/app/api/stats/categories/route';
+import { Card, CardHeader, CardTitle } from '@/components/ui/card';
 
 interface Props{
     userSettings:UserSettings;
@@ -10,11 +16,58 @@ interface Props{
 }
 
 function CategoriesStats({userSettings,from,to}:Props) {
+
+  const statsQuery = useQuery<GetCategoriesStatsResponseType>({
+    queryKey:["overview","stats","categories",from ,to ],
+    queryFn: () => fetch(`/api/stats/categories?from=${DateToUTCDate(from)}&to=${DateToUTCDate(to)}`).then (res => res.json()),
+  });
+
+  const formatter = useMemo(() => {
+    return GetFormatterForCurrency(userSettings.currency)
+  },[userSettings.currency])
+
   return (
-    <div>
-    
+    <div className='flex w-full flex-wrap gap-2 md:flex-nowrap'>
+      <SkeletonWrapper
+        isLoading={statsQuery.isFetching}
+      >
+        <CategoriesCard
+          formatter={formatter}
+          type="income"
+          data={statsQuery.data || []}
+        />
+      </SkeletonWrapper>
     </div>
   )
 }
 
 export default CategoriesStats
+
+
+function CategoriesCard({data,type,formatter}:{
+  type:TransactionType,
+  formatter: Intl.NumberFormat,
+  data: GetCategoriesStatsResponseType
+}){
+  const filteredData = data.filter(el => el.type === type);
+  const total =  filteredData.reduce((acc,el) => acc + (el._sum?.amount || 0),0)
+  return (
+    <Card className='h-80 w-full col-span-6'>
+      <CardHeader>
+        <CardTitle className='grid grid-flow-row justify-between gap-2 text-muted-foreground md:grid-flow-col'>
+
+        </CardTitle>
+      </CardHeader>
+      <div className="flex items-center justify-between gap-2">
+        {filteredData.length === 0 && (
+          <div className='flex h-60 w-full flex-col items-center justify-center'>
+            No data for the selected period
+            <p className='text-sm text-muted-foreground'>
+              Try selecting a different period or try adding new{" "} {type === "income"? "incomes" : "expenses"}
+            </p>
+          </div>
+        )}
+      </div>
+    </Card>
+  );
+}

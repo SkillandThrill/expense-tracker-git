4ad45f12433ff49ad618803fa82120c291164fb3
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-20 01:02:03 +0530

    added the custom tooltip with XAxis and YAxis props into the bar

--- app/(dashboard)/_components/History.tsx
+++ app/(dashboard)/_components/History.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { UserSettings } from '@prisma/client'
-import React, { useMemo, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import { Timeframe } from '@/lib/types';
 import { Period } from '@/lib/types';
 import { GetFormatterForCurrency } from '@/lib/helpers';
@@ -19,6 +19,8 @@ import {
     XAxis,
     YAxis,
 } from "recharts"
+import { cn } from '@/lib/utils';
+import CountUp from 'react-countup';
 
 function History({userSettings}:{userSettings:UserSettings}) {
 
@@ -77,6 +79,27 @@ function History({userSettings}:{userSettings:UserSettings}) {
                                     </linearGradient>
                                 </defs>
                                 <CartesianGrid strokeDasharray="5 5" strokeOpacity={"0.2"} vertical={false}/>
+                                <XAxis stroke='#888888' fontSize={12} tickLine={false} axisLine={false} padding={{left:5,right:5}} dataKey={(data) =>{
+                                    const {year,month,day}=data;
+                                    const date = new Date(year,month,day ||1)
+                                    if(timeframe === "year"){
+                                        return date.toLocaleDateString("default",{
+                                                month:"long"
+                                            });
+                                        }
+                                        return date.toLocaleDateString("default",{
+                                            day:"2-digit"
+                                        })
+                                    }}
+                                />
+                                <YAxis
+                                    stroke='#888888' fontSize={12} tickLine={false} axisLine={false}
+                                />
+                                <Bar dataKey={"income"} label="Income" fill='url(#incomeBar)' radius={4} className='cursor-pointer'/>
+                                <Bar dataKey={"expense"} label="Expense" fill='url(#expenseBar)' radius={4} className='cursor-pointer'/>
+                                <Tooltip cursor={{opacity:0.1}} content={props=>(
+                                    <CustomTooltip formatter={formatter}{...props}/>
+                                )}/>
                             </BarChart>
                         </ResponsiveContainer>}
                     {!dataAvailable && (
@@ -95,3 +118,52 @@ function History({userSettings}:{userSettings:UserSettings}) {
 }
 
 export default History
+
+
+function CustomTooltip({active,payload, formatter} : any){
+    if(!active || !payload || payload.length === 0) return null;
+
+    const data = payload[0].payload;
+    const {expense, income} = data;
+
+    return(
+        <div className="min-w-[300px] rounded border bg-background p-4 ">
+            <TooltipRow formatter={formatter} label="Expense" value={expense} bgColor="bg-red-500" textColor= "text-red-500"/>
+            <TooltipRow formatter={formatter} label="Income" value={income} bgColor="bg-emerald-500" textColor= "text-emerald-500"/>
+            <TooltipRow formatter={formatter} label="Balance" value={income - expense} bgColor="bg-gray-100" textColor= "text-foreground"/>
+        </div>
+    )
+}
+
+
+function TooltipRow({
+    label,value, bgColor,textColor,formatter
+}:{
+    label:string;
+    textColor:string;
+    bgColor:string;
+    value:number;
+    formatter:Intl.NumberFormat;
+}){
+
+    const formattingFn = useCallback((value:number) => {
+        return formatter.format(value)
+    },[formatter])
+    return(
+        <div className="flex items-center gap-2">
+            <div className={cn(
+            "h-4 w-4 rounded-full", bgColor
+            )}/>
+            <div className="flex w-full justify-between">
+                <p className='text-sm text-muted-foreground'>
+                    {label}
+                </p>
+                <div className={cn(
+                    "text-sm font-bold",textColor
+                )}>
+                    <CountUp duration={0.5} preserveValue end={value} decimals={0} formattingFn={value => formatter.format(value)} className='text-sm'/>
+                </div>
+            </div>
+        </div>
+    )
+}

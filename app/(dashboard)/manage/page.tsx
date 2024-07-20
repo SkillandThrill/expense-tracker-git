5bfa7c46a7dfc7bc6d0e5810007dd87aa36a05ff
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-21 01:08:18 +0530

    to select the default cuurency added the currrency combobox component to select the currency . defining the categoryList coponents which accepyts the type props: as income and expense.

--- app/(dashboard)/manage/page.tsx
+++ app/(dashboard)/manage/page.tsx
@@ -1,11 +1,18 @@
 "use client"
 
 
+import { CurrencyComboBox } from '@/components/CurrencyComboBox'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import React from 'react'
+import { Calendar } from '@/components/ui/calendar';
+import { TransactionType } from '@/lib/types';
+import { useQuery } from '@tanstack/react-query';
+import SkeletonWrapper from '@/components/SkeletonWrapper';
 
 function page() {
   return (
     <>
+    {/* Header */}
         <div className="border-b bg-card">
             <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
                 <div className="">
@@ -14,8 +21,42 @@ function page() {
                 </div>
             </div>
         </div>
+        {/* End header */}
+
+        <div className="container flex flex-col gap-4 p-4">
+            <Card>
+                <CardHeader>
+                    <CardTitle>
+                        Currency
+                    </CardTitle>
+                    <CardDescription>
+                        Set your default currency for transactions
+                    </CardDescription>
+                </CardHeader>
+
+                <CardContent>
+                    <CurrencyComboBox/>
+                </CardContent>
+            </Card>
+
+            <CategoryList type="income"/>
+            <CategoryList type="expense"/>
+        </div>
     </>
   )
 }
 
 export default page
+
+function  CategoryList({type}:{type:TransactionType}){
+    const categoriesQuery = useQuery({
+        queryKey:["categories",type],
+        queryFn: () => fetch(`api/categories?type=${type}`).then(res => res.json())
+    });
+
+    return (
+        <SkeletonWrapper isLoading>
+
+        </SkeletonWrapper>
+    )
+}

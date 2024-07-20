b3e09c3eaf423880f212d0b4e2d2f430ebee5f27
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-21 00:58:00 +0530

    started creating manage page for our app within dashboard

---
+++ app/(dashboard)/manage/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+
+import React from 'react'
+
+function page() {
+  return (
+    <>
+        <div className="border-b bg-card">
+            <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+                <div className="">
+                    <p className='text-3xl font-bold'>Manage</p>
+                    <p className="text-muted-foreground">Manage your account settings and categories</p>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default page

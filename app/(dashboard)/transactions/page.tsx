3057bcef8434d40ae8956f44fa24d943394b9f60
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-23 02:37:53 +0530

    added the header with the styling of the card

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -1,1 +1,19 @@
 "use client"
+import React from 'react'
+import History from '../_components/History';
+
+function page() {
+  return (
+    <div className='border-b bg-card'>
+        <div className="container flex items-center flex-wrap justify-between gap-6 py-8">
+            <div className="">
+                <p className="text-3xl font-bold">
+                    Transaction history
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default page

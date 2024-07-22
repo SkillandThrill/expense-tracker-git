a71cd1fd59ad5c80946aa617b5a16f670c0d1fa6
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-23 02:48:34 +0530

    now we used the dateRangepicker and copied the code from overview component..imported relateable props and added states for the daterange. added properly alligned styling to the datepicker and the heading

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -1,8 +1,17 @@
 "use client"
-import React from 'react'
-import History from '../_components/History';
 
-function page() {
+import React, { useState } from 'react'
+import { DateRangePicker } from '@/components/ui/date-range-picker';
+import { differenceInDays, startOfMonth } from 'date-fns';
+import { MAX_DATE_RANGE_DAYS } from '@/lib/constants';
+import {toast} from "sonner"
+
+function TransactionsPage() {
+    const [dateRange,setDateRange] = useState<{from:Date; to : Date}>({
+        from: startOfMonth( new Date()),
+        to: new Date(),
+    })
+
   return (
     <div className='border-b bg-card'>
         <div className="container flex items-center flex-wrap justify-between gap-6 py-8">
@@ -11,9 +20,26 @@ function page() {
                     Transaction history
                 </p>
             </div>
+            <DateRangePicker
+                    initialDateFrom={dateRange.from}
+                    initialDateTo={dateRange.to}
+                    showCompare ={false}
+                    onUpdate={values =>{
+                        const {from,to}= values.range;
+                        // we update date range only if oth dates are set
+                        if(!from || !to ) return;
+
+                        if(differenceInDays(to,from) > MAX_DATE_RANGE_DAYS){
+                            toast.error(`The selected date range is too big. Max allowed range is ${MAX_DATE_RANGE_DAYS} days!`);
+                            return;
+                        }
+
+                        setDateRange({from,to});
+                    }}
+                />
         </div>
     </div>
   )
 }
 
-export default page
+export default TransactionsPage

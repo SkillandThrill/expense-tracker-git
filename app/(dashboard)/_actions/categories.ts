22c239c10961456c3604b7c4bad19664c7e11aba
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-22 16:39:28 +0530

    need to define a api to delete the category.. defining the deleteCategorySchenaType exported the delete schena type server actrion and the schema type. and imported them to deleteaCategorydialog.. used mutation to return the toast after the succss callback

--- app/(dashboard)/_actions/categories.ts
+++ app/(dashboard)/_actions/categories.ts
@@ -1,12 +1,14 @@
 "use server";
 
-import { CreateCategorySchemaType } from "@/schema/categories";
+import { CreateCategorySchemaType, DeleteCategorySchema } from "@/schema/categories";
 import { CreateCategorySchema } from '../../../schema/categories';
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import prisma from "@/lib/prisma";
+import { DeleteCategorySchemaType } from "@/schema/categories";
 
 export async function CreateCategory(form:CreateCategorySchemaType){
+
     const parsedBody = CreateCategorySchema.safeParse(form);
 
     if(!parsedBody.success){
@@ -28,4 +30,28 @@ export async function CreateCategory(form:CreateCategorySchemaType){
             type,
         },
     });
+}
+
+export async function DeleteCategory(form: DeleteCategorySchemaType){
+    const parsedBody = DeleteCategorySchema.safeParse(form);
+
+    if(!parsedBody.success){
+        throw new Error("bad request");
+    }
+
+    const user =  await currentUser();
+    if(!user){
+        redirect("/sign-in")
+    }
+
+    return await prisma.category.delete({
+        where:{
+            name_userId_type:{
+                userId:user.id,
+                name:parsedBody.data.name,
+                type:parsedBody.data.type
+            }
+        }
+    })
+
 }
--- app/(dashboard)/_components/DeleteCategoryDialog.tsx
+++ app/(dashboard)/_components/DeleteCategoryDialog.tsx
@@ -1,9 +1,32 @@
 "use client"
-import React from 'react'
+import { Category } from '@prisma/client';
+import { useMutation } from '@tanstack/react-query';
+import React, { ReactNode } from 'react'
+import { DeleteCategory } from '../_actions/categories';
+import { toast } from 'sonner';
 
-function DeleteCategoryDialog() {
 
+interface Props{
+    trigger:ReactNode;
+    category:Category;
+
+}
+
+function DeleteCategoryDialog({category,trigger}:Props) {
+
+    const categoryIdentifier = `${category.name}-${category.type}`
     
+
+    const deleteMutation = useMutation({
+        mutationFn: DeleteCategory,
+        onSuccess: () =>{
+            toast.success("Category deleted successfully",{
+                id:categoryIdentifier,
+
+            })
+
+        }
+    })
 
   return (
     <div>

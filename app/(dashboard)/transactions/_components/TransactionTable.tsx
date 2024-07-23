cb2dad07faf7afaad59288b4c2f004f1e8a68c8d
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-23 15:56:58 +0530

    started creating the table for the transaction data and for the table using shadcn ui data table which relise on tanstack table.. so installed the package and proceeded with building

--- app/(dashboard)/transactions/_components/TransactionTable.tsx
+++ app/(dashboard)/transactions/_components/TransactionTable.tsx
@@ -3,12 +3,19 @@
 import { DateToUTCDate } from '@/lib/helpers';
 import { useQuery } from '@tanstack/react-query';
 import React from 'react'
+import {ColumnDef} from "@tanstack/react-table"
+import { GetTransactionHistoryResponseType } from '@/app/api/transactions-history/route';
 
 interface Props{
     from:Date;
     to:Date;
 }
 
+type TransactionHistoryRow = GetTransactionHistoryResponseType[0]
+
+
+
+export const columns:ColumnDef<TransactionHistoryRow>[] =[];
 function TransactionTable({from,to}:Props) {
 
     const history = useQuery({
@@ -16,6 +23,8 @@ function TransactionTable({from,to}:Props) {
         queryFn: () => fetch(`/api/transactions-history?from=${DateToUTCDate(from)}&to=${DateToUTCDate(to)}`).then(res => res.json())
     })
   return (
+
+    
     <div>
       
     </div>

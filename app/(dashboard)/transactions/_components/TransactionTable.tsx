cb143ace2393d435500bbb8f6d1bcdb1df1bab20
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-24 01:40:43 +0530

    while creating the table adding the rows and columns to the table for that importing rest of the table properties

--- app/(dashboard)/transactions/_components/TransactionTable.tsx
+++ app/(dashboard)/transactions/_components/TransactionTable.tsx
@@ -3,7 +3,16 @@
 import { DateToUTCDate } from '@/lib/helpers';
 import { useQuery } from '@tanstack/react-query';
 import React from 'react'
-import {ColumnDef} from "@tanstack/react-table"
+import {ColumnDef, flexRender,getCoreRowModel,useReactTable} from "@tanstack/react-table"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
 import { GetTransactionHistoryResponseType } from '@/app/api/transactions-history/route';
 
 interface Props{
@@ -15,10 +24,24 @@ type TransactionHistoryRow = GetTransactionHistoryResponseType[0]
 
 
 
-export const columns:ColumnDef<TransactionHistoryRow>[] =[];
+export const columns:ColumnDef<TransactionHistoryRow>[] =[
+    {
+        accessorKey: "category",
+        cell:({row})=> (
+            <div className='flex gap-2 capitalize'>
+                {row.original.categoryIcon}
+                <div className="capitalize">
+                    {row.original.category}
+                </div>
+            </div>
+        ),
+    },
+
+];
+
 function TransactionTable({from,to}:Props) {
 
-    const history = useQuery({
+    const history = useQuery<GetTransactionHistoryResponseType>({
         queryKey: ["transactions","history",from,to],
         queryFn: () => fetch(`/api/transactions-history?from=${DateToUTCDate(from)}&to=${DateToUTCDate(to)}`).then(res => res.json())
     })

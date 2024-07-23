0667c260de4f7a20d476fd1b64f67ff031cc8c2b
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-23 15:35:07 +0530

    created the api and called it in TranasctionTable component

--- app/(dashboard)/transactions/_components/TransactionTable.tsx
+++ app/(dashboard)/transactions/_components/TransactionTable.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { DateToUTCDate } from '@/lib/helpers';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 
 interface Props{
@@ -8,6 +10,11 @@ interface Props{
 }
 
 function TransactionTable({from,to}:Props) {
+
+    const history = useQuery({
+        queryKey: ["transactions","history",from,to],
+        queryFn: () => fetch(`/api/transactions-history?from=${DateToUTCDate(from)}&to=${DateToUTCDate(to)}`).then(res => res.json())
+    })
   return (
     <div>
       

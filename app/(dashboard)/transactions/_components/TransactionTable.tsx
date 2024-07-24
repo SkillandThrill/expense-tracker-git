d7d978dbc88b9eafb3760ccf8c929cc29f5e9c73
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-07-25 00:25:23 +0530

    added the complete skeletal of the table including the cells columns and the rows

--- app/(dashboard)/transactions/_components/TransactionTable.tsx
+++ app/(dashboard)/transactions/_components/TransactionTable.tsx
@@ -79,7 +79,26 @@ function TransactionTable({from,to}:Props) {
                     ))}
                 </TableHeader>
                 <TableBody>
-                    
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map((row) =>(
+                            <TableRow
+                                key={row.id}
+                                data-state={row.getIsSelected() && "selected"}
+                            >
+                                {row.getVisibleCells().map((cell)=>(
+                                    <TableCell key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell,cell.getContext())}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell colSpan={columns.length} className='h-24 text-center'>
+                                No results.
+                            </TableCell>
+                        </TableRow>
+                    )}
                 </TableBody>
             </Table>
         </SkeletonWrapper>
